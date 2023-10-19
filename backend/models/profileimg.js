62299b7a372b92c7286254a2df3ee01d65f8bd5f
OmkarBoralkar1 <omkarboralkar@gmail.com>
2023-10-19 10:12:35 +0530

    cdvfvfbff

--- backend/models/profileimg.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const profileimgschema = new mongoose.Schema({
-  username: String,
-  image:String,
-  origimg:String,
-});
-const profileimgmodel = mongoose.model('profileimg', profileimgschema);
-module.exports =  profileimgmodel;
